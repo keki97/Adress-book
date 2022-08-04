be9a148a434ad8de7d30b16b9173895b4a7793de
aleksandar kekerovic <aleksandar.kekerovic97@gmail.com>
2022-08-04 14:53:35 +0200

    Style table and form

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
+"use strict";
 
+const addBtn = document.querySelector(".add-person");
+const addModal = document.querySelector(".form-container");
+
+addBtn.addEventListener("click", function () {
+  addModal.style.display = "block";
+});
