05225f7251812a6f6d6b4ced2c44b15eba82b9a4
aleksandar kekerovic <aleksandar.kekerovic97@gmail.com>
2022-08-08 13:19:27 +0200

    Improve add person

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 "use strict";
 
+const addressBook = [];
+
 const addBtn = document.querySelector(".add-person");
 const addModal = document.querySelector(".form-container");
 const closeModal = document.querySelector(".close");
@@ -10,6 +12,7 @@ const emailInput = document.getElementById("email");
 const phoneInput = document.getElementById("phone-number");
 const tableInput = document.querySelector(".table-body");
 let html;
+
 addBtn.addEventListener("click", function () {
   addModal.style.display = "block";
 });
@@ -18,19 +21,42 @@ closeModal.addEventListener("click", function () {
   addModal.style.display = "none";
 });
 
-submitBtn.addEventListener("click", function () {
+const insertHTML = function (el, i) {
   const html = `
-          <tr>  
-            <td>${nameInput.value}</td>
-            <td>${lastNameInput.value}</td>
-            <td>${emailInput.value}</td>
-            <td>${phoneInput.value}</td>
-          </tr>
-  `;
+  <tr>
+    <td>${i + 1}</td>
+    <td>${el.firstName}</td>
+    <td>${el.lastName}</td>
+    <td>${el.email}</td>
+    <td>${el.phoneNumber}</td>
+  </tr>
+`;
 
   tableInput.insertAdjacentHTML("beforeend", html);
+};
 
+// const displayHTML = function () {
+//   for (let i = 0; i < addressBook.length; i++) {
+//     insertHTML(addressBook[i], i);
+//   }
+// };
+
+const add = function (newPerson) {
+  addressBook.push(newPerson);
+  insertHTML(newPerson, addressBook.length - 1);
+};
+
+submitBtn.addEventListener("click", function () {
   addModal.style.display = "none";
+
+  const newPerson = {
+    firstName: nameInput.value,
+    lastName: lastNameInput.value,
+    email: emailInput.value,
+    phoneNumber: phoneInput.value,
+  };
+
+  add(newPerson);
 
   nameInput.value = "";
   lastNameInput.value = "";
