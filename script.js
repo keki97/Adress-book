aa8d2f8b9795710c983717142de0269fa76ee147
aleksandar kekerovic <aleksandar.kekerovic97@gmail.com>
2022-08-12 14:25:12 +0200

    Validation functionality

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ const addModal = document.querySelector(".form-container");
 const toggleOverlay = document.querySelector(".form-overlay");
 const form = document.querySelector(".form");
 const addEditModal = document.querySelector(".edit-form-container");
+const editForm = document.querySelector(".form-edit");
 const closeModal = document.querySelectorAll(".close");
 const submitBtn = document.querySelector(".btn-submit");
 const tableEl = document.querySelector(".table");
@@ -33,6 +34,16 @@ const close = function () {
 closeModal.forEach((el) => el.addEventListener("click", close));
 
 toggleOverlay.addEventListener("click", close);
+
+const isEmail = function (email) {
+  return /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+    email
+  );
+};
+
+const isPhoneNumber = function (phone) {
+  return /^(\+\d{1,3}\s)?\(?\d{2,3}\)?[\s-]\d{3}[\s-]\d{3,4}$/.test(phone);
+};
 
 // Add Person
 
@@ -63,24 +74,9 @@ const displayHTML = function () {
 
 // Submit changes
 
-submitBtn.addEventListener("click", function (e) {
+form.addEventListener("submit", function (e) {
   e.preventDefault();
-  addModal.style.display = "none";
-  toggleOverlay.style.display = "none";
-
-  const newPerson = {
-    firstName: nameInput.value,
-    lastName: lastNameInput.value,
-    email: emailInput.value,
-    phoneNumber: phoneInput.value,
-  };
-
-  add(newPerson);
-
-  nameInput.value = "";
-  lastNameInput.value = "";
-  emailInput.value = "";
-  phoneInput.value = "";
+  checkInputs();
 });
 
 // Local Storage
@@ -108,7 +104,6 @@ tableEl.addEventListener("click", function (e) {
     addressBook.splice(id, 1);
     tableInput.innerHTML = "";
     displayHTML();
-    console.log(addressBook);
   }
 });
 
@@ -140,9 +135,9 @@ tableEl.addEventListener("click", function (e) {
     emailEdit.value = document.getElementById("email-" + id).textContent;
     phoneEdit.value = document.getElementById("phone-" + id).textContent;
   }
-  saveEdit.addEventListener("click", function (e) {
-    // e.preventDefault();
-    console.log(e);
+  editForm.addEventListener("submit", function (e) {
+    e.preventDefault();
+    // checkEditInputs();
     addressBook[id].firstName = document.getElementById(
       "first-name-" + id
     ).textContent = nameEdit.value;
@@ -161,10 +156,133 @@ tableEl.addEventListener("click", function (e) {
 
 // FORM VALIDATION
 
-if (nameInput.value.length >= 3 && nameInput.value.length <= 20) {
-} else {
-}
+const checkInputs = function () {
+  const nameValue = nameInput.value.trim();
+  const lastNameValue = lastNameInput.value.trim();
+  const emailValue = emailInput.value.trim();
+  const phoneNumberValue = phoneInput.value.trim();
+  const nameErrorMessage = document.getElementById("name-error");
+  const lastNameErrorMessage = document.getElementById("last-name-error");
+  const emailErrorMessage = document.getElementById("email-error");
+  const phoneErrorMessage = document.getElementById("phone-error");
 
-if (lastNameInput.value.length >= 3 && nameInput.value.length <= 20) {
-} else {
-}
+  if (nameValue.length < 3 || nameValue.length > 20) {
+    nameErrorMessage.textContent =
+      "Name needs to have between 3 and 20 characters";
+    nameInput.classList.add("error-input");
+  } else {
+    nameErrorMessage.textContent = "";
+  }
+
+  if (lastNameValue.length < 3 || lastNameValue.length > 20) {
+    lastNameErrorMessage.textContent =
+      "Last name needs to have between 3 and 20 characters";
+  } else {
+    lastNameErrorMessage.textContent = "";
+  }
+
+  if (emailValue === "") {
+    emailErrorMessage.textContent = "Email cannot be empty";
+  } else if (!isEmail(emailValue)) {
+    emailErrorMessage.textContent = "Email is not valid";
+  } else {
+    emailErrorMessage.textContent = "";
+  }
+
+  if (phoneNumberValue === "") {
+    phoneErrorMessage.textContent = "Phone number cannot be empty";
+  } else if (!isPhoneNumber(phoneNumberValue)) {
+    phoneErrorMessage.textContent =
+      "Phone number is not valid. Try: +### ## ### ### or ### ### ### ";
+  } else {
+    phoneErrorMessage.textContent = "";
+  }
+
+  if (
+    nameErrorMessage.textContent === "" &&
+    lastNameErrorMessage.textContent === "" &&
+    emailErrorMessage.textContent === "" &&
+    phoneErrorMessage.textContent === ""
+  ) {
+    addModal.style.display = "none";
+    toggleOverlay.style.display = "none";
+
+    const newPerson = {
+      firstName: nameInput.value,
+      lastName: lastNameInput.value,
+      email: emailInput.value,
+      phoneNumber: phoneInput.value,
+    };
+
+    add(newPerson);
+
+    nameInput.value = "";
+    lastNameInput.value = "";
+    emailInput.value = "";
+    phoneInput.value = "";
+  }
+};
+
+// const checkEditInputs = function () {
+//   const nameValue = nameEdit.value.trim();
+//   const lastNameValue = lastNameEdit.value.trim();
+//   const emailValue = emailEdit.value.trim();
+//   const phoneNumberValue = phoneEdit.value.trim();
+//   const nameErrorMessage = document.getElementById("name-error-edit");
+//   const lastNameErrorMessage = document.getElementById("last-name-error-edit");
+//   const emailErrorMessage = document.getElementById("email-error-edit");
+//   const phoneErrorMessage = document.getElementById("phone-error-edit");
+
+//   if (nameValue.length < 3 || nameValue.length > 20) {
+//     nameErrorMessage.textContent =
+//       "Name needs to have between 3 and 20 characters";
+//     nameInput.classList.add("error-input");
+//   } else {
+//     nameErrorMessage.textContent = "";
+//   }
+
+//   if (lastNameValue.length < 3 || lastNameValue.length > 20) {
+//     lastNameErrorMessage.textContent =
+//       "Last name needs to have between 3 and 20 characters";
+//   } else {
+//     lastNameErrorMessage.textContent = "";
+//   }
+
+//   if (emailValue === "") {
+//     emailErrorMessage.textContent = "Email cannot be empty";
+//   } else if (!isEmail(emailValue)) {
+//     emailErrorMessage.textContent = "Email is not valid";
+//   } else {
+//     emailErrorMessage.textContent = "";
+//   }
+
+//   if (phoneNumberValue === "") {
+//     phoneErrorMessage.textContent = "Phone number cannot be empty";
+//   } else if (!isPhoneNumber(phoneNumberValue)) {
+//     phoneErrorMessage.textContent =
+//       "Phone number is not valid. Try: +### ## ### ### or ### ### ### ";
+//   } else {
+//     phoneErrorMessage.textContent = "";
+//   }
+
+//   if (
+//     nameErrorMessage.textContent === "" &&
+//     lastNameErrorMessage.textContent === "" &&
+//     emailErrorMessage.textContent === "" &&
+//     phoneErrorMessage.textContent === ""
+//   ) {
+//     addressBook[id].firstName = document.getElementById(
+//       "first-name-" + id
+//     ).textContent = nameEdit.value;
+//     addressBook[id].lastName = document.getElementById(
+//       "last-name-" + id
+//     ).textContent = lastNameEdit.value;
+//     addressBook[id].email = document.getElementById("email-" + id).textContent =
+//       emailEdit.value;
+//     addressBook[id].phoneNumber = document.getElementById(
+//       "phone-" + id
+//     ).textContent = phoneEdit.value;
+//     close();
+//     window.location.reload();
+//   }
+// };
